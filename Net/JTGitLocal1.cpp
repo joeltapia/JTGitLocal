817b066325dc094575878a7e7a970fe41c6b7cb5
jtapi <jtapi@LAPTOP-TV76NM6C>
2025-04-24 19:50:00 -0500

    Segundo cambio

---
+++ Net/JTGitLocal1.cpp
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cpplite/CPPTemplate.cpp to edit this template
+ */
+System.out.println("Impresión de números pares:");
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.println(i);
+        }
+
+        System.out.println("Impresión de números impares:");
+        for (int i = 1; i < 20; i += 2) {
+            System.out.println(i);
+        }
+
+        System.out.println("Cuenta regresiva:");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        
+System.out.println("Este es el segundo cambio");
