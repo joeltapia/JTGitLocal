b08afca908a53ba1c40cad19e1bacdc0cca88c09
jtapi <jtapi@LAPTOP-TV76NM6C>
2025-04-24 19:26:05 -0500

    Primer cambio

---
+++ Net/JTGitLocal.cpp
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cpplite/CPPTemplate.cpp to edit this template
+ */
+System.out.println("Impresión de números pares:");
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.println(i);
+        }
+
+        System.out.println("Impresión de números impares:");
+        for (int i = 1; i < 20; i += 2) {
+            System.out.println(i);
+        }
